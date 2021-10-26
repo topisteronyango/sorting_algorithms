66c681fc4477e8f23511fdfd3b4bcfd03db4c1cf
topister onyango <topisternandera@gmail.com>
2021-10-26 12:34:26 -0700

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,26 +8,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, idy, min_idy;
-	int tmp, switched = 0;
+	size_t i, idx, min_idx;
+	int tmp, swapped = 0;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		switched = 0;
-		min_idy = i;
-		for (idy = i + 1; idy < size; idy++)
+		swapped = 0;
+		min_idx = i;
+		for (idx = i + 1; idx < size; idx++)
 		{
-			if (array[idy] < array[min_]idy])
+			if (array[idx] < array[min_idx])
 			{
-				min_idy = idy;
-				switched = 1;
+				min_idx = idx;
+				swapped = 1;
 			}
 		}
-		if (switched)
+		if (swapped)
 		{
 			tmp = array[i];
-			array[i] = array[min_idy];
-			array[min_idy] = tmp;
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
 			print_array(array, size);
 		}
 	}
