d19d30fc892556e68cb75a129d1199ec3de4e25d
topister onyango <topisternandera@gmail.com>
2021-10-26 12:45:28 -0700

    header file

--- sort.h
+++ sort.h
@@ -27,6 +27,9 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 
+/* auxiliar functions */
+void swap(int *array, size_t a, size_t b);
+
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
